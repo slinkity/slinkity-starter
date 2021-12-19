6f657f6e4f1a4a1ba66674efe5a1b559aedd6c81
Holben888 <bholmesdev@gmail.com>
2021-12-19 18:55:27 -0500

    feat: prompt for project name + set package name

--- cli.js
+++ cli.js
@@ -2,30 +2,59 @@
 const path = require('path')
 const fs = require('fs')
 const { yellow, red } = require('kolorist')
+const prompts = require('prompts')
 
-console.log('Copying files...')
+const PKG = 'package.json'
 
-// TODO: make edit-able via CLI
-const target = 'my-slinkity'
-const srcRoot = path.join(__dirname, 'templates/react')
-const destRoot = path.join(process.cwd(), target)
-fs.mkdirSync(destRoot)
-// TODO: add "plain" starter alongside "react"
-const templateFilePaths = fs.readdirSync(srcRoot)
+;(async () => {
+  let dest = process.argv[2]
+  const promptResponses = await prompts({
+    type: dest ? null : 'text',
+    name: 'dest',
+    message: 'What is your project called?',
+    initial: 'my-slinkity-site',
+  })
+  if (!dest) {
+    dest = promptResponses.dest
+  }
+  // if there's no dest provided as a CLI argument OR by a prompt,
+  // they must have ctrl + C'd out of the program
+  if (!dest) process.exit(0)
 
-for (const templateFilePath of templateFilePaths) {
-  const src = path.join(srcRoot, templateFilePath)
-  const dest = path.join(destRoot, templateFilePath)
-  copy(src, dest)
-}
+  console.log('Copying files...')
+
+  const srcRoot = path.join(__dirname, 'templates/react')
+  const destRoot = path.join(process.cwd(), dest)
+  fs.mkdirSync(destRoot)
+
+  // Set package name based on project name
+  const pkg = require(path.join(srcRoot, PKG))
+  pkg.name = toValidPackageName(dest)
+  fs.writeFileSync(path.join(destRoot, PKG), JSON.stringify(pkg, null, 2))
+
+  // Copy all template files to dest
+  // TODO: add "plain" starter alongside "react"
+  const templateFilePaths = fs.readdirSync(srcRoot).filter(filePath => filePath !== PKG)
+  for (const templateFilePath of templateFilePaths) {
+    const src = path.join(srcRoot, templateFilePath)
+    const dest = path.join(destRoot, templateFilePath)
+    copy(src, dest)
+  }
+
+  console.log(`Welcome to your first ${yellow('Slinkity site!')}`)
+  console.log('Step 1: run these commands to install and serve locally:')
+  console.log(`yarn
+yarn start`)
+  console.log(`Step 2: ${red('have fun ❤️')}`)
+})()
 
 /**
- * Util to copy file or file directory to dest
- * Taken from https://github.com/vitejs/vite/blob/main/packages/create-vite/index.js
- * @param {string} src
- * @param {string} dest
- */
-function copy(src, dest) {
+   * Util to copy file or file directory to dest
+   * Inspired by https://github.com/vitejs/vite/blob/main/packages/create-vite/index.js
+   * @param {string} src
+   * @param {string} dest
+   */
+ function copy(src, dest) {
   const stat = fs.statSync(src)
   if (stat.isDirectory()) {
     copyDir(src, dest)
@@ -36,7 +65,7 @@ function copy(src, dest) {
 
 /**
  * Util to copy file directory to test
- * Taken from https://github.com/vitejs/vite/blob/main/packages/create-vite/index.js
+ * Inspired by https://github.com/vitejs/vite/blob/main/packages/create-vite/index.js
  * @param {string} srcDir 
  * @param {string} destDir 
  */
@@ -49,8 +78,17 @@ function copyDir(srcDir, destDir) {
   }
 }
 
-console.log(`Welcome to your first ${yellow('Slinkity site!')}`)
-console.log('Step 1: run these commands to install and serve locally:')
-console.log(`yarn
-yarn start`)
-console.log(`Step 2: ${red('have fun ❤️')}`)
+/**
+ * Util to convert a project name into a valid package name for a package.json
+ * Inspired by https://github.com/vitejs/vite/blob/main/packages/create-vite/index.js
+ * @param {string} projectName 
+ * @returns {string} A valid package name
+ */
+function toValidPackageName(projectName) {
+  return projectName
+    .trim()
+    .toLowerCase()
+    .replace(/\s+/g, '-')
+    .replace(/^[._]/, '')
+    .replace(/[^a-z0-9-~]+/g, '-')
+}
